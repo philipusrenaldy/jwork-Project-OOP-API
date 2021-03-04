02d0a483c5a2dfe76608aa35d57602e0516dd05c
philipusrenaldy <philipusrenaldy@gmail.com>
2021-03-04 17:35:15 +0700

    modul 1

---
+++ jwork/Recruiter.java
@@ -0,0 +1,49 @@
+
+public class Recruiter
+{
+    // instance variables - replace the example below with your own
+    private int id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    /**
+     * Constructor for objects of class Recruiter
+     */
+    public Recruiter()
+    {
+    
+    }
+    public static int getId()
+    {
+        return 0;
+    }
+    public static String getName()
+    {
+        return "oop";
+    }
+    public static String getEmail()
+    {
+        return "oop";
+    }
+    public static String getPhoneNumber()
+    {
+        return "oop";
+    }
+    public static void setId(int id)
+    {
+        
+    }
+    public static void setEmail(String email)
+    {
+        
+    }
+    public static void setName(String name)
+    {
+        
+    }
+    public static void setPhoneNumber(String phoneNumber)
+    {
+        
+    }
+}
